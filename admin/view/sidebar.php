04cb99592113685931f434dd65a07bc437b9de35
tieutuan <tieuhoangtuan822001@gmail.com>
2022-03-18 21:14:41 +0700

    CS-2: Admin Layout

---
+++ admin/view/sidebar.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <input type="checkbox" id="nav-toggle">
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <h2><span class="lab la-accusoft"> Coffee Shop</span></h2>
+        </div>
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="#"><span class="las la-igloo"></span>
+                    <span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="las la-users"></span>
+                    <span>Customers</span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="las la-clipboard-list"></span>
+                    <span>Projects</span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="las la-shopping-bag"></span>
+                    <span>Orders</span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="las la-receipt"></span>
+                    <span>Inventory</span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="las la-user-circle"></span>
+                    <span>Accounts</span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="las la-clipboard"></span>
+                    <span>Tasks</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
