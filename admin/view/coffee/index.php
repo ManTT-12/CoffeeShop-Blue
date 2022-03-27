ec249bafc0c08a0becad7b69752c4d933934b026
tieutuan <tieuhoangtuan822001@gmail.com>
2022-03-27 11:59:00 +0700

    CS-13 Show coffe list

--- admin/view/coffee/index.php
+++ admin/view/coffee/index.php
@@ -1,5 +1,24 @@
 <?php require_once('view/layout/header.php'); ?>
 <!-- Start Content -->
+<div class="table-responsive">
+    <table id="data-table" class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Image</td>
+                <td>Type</td>
+                <td>Price</td>
+                <td>Status</td>
+                <td>Created date</td>
+                <td>Updated date</td>
+                <td>Action</td>
+            </tr>
+        </thead>
+        <tbody>
 
+        </tbody>
+    </table>
+</div>
+<script src="js/coffee/list.js"></script>
 <!-- End Content -->
 <?php require_once('view/layout/footer.php'); ?>
