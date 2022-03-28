736b21fa843233e6cd369b10306a1f9e06585e55
nguyencaothai <lanhoxga@gmail.com>
2022-03-28 13:11:04 +0700

    CS-24:Login UI

--- admin/view/admin/dashboard.php
+++ admin/view/admin/dashboard.php
@@ -3,39 +3,39 @@
 <div class="cards">
     <div class="card-single">
         <div>
-                        <h1>54</h1>
-                        <span>Customers</span>
+            <h1>54</h1>
+            <span>Customers</span>
         </div>
         <div>
-                        <span class="las la-users"></span>
+            <span class="las la-users"></span>
         </div>
     </div>
     <div class="card-single">
         <div>
-                        <h1>79</h1>
-                        <span>Projects</span>
+            <h1>79</h1>
+            <span>Projects</span>
         </div>
         <div>
-                        <span class="las la-clipboard-list"></span>
+            <span class="las la-clipboard-list"></span>
         </div>
     </div>
     <div class="card-single">
-                    <div>
-                        <h1>124</h1>
-                        <span>Orders</span>
-                    </div>
-                    <div>
-                        <span class="las la-shopping-bag"></span>
-                    </div>
+        <div>
+            <h1>124</h1>
+            <span>Orders</span>
+        </div>
+        <div>
+            <span class="las la-shopping-bag"></span>
+        </div>
     </div>
     <div class="card-single">
-                    <div>
-                        <h1>$6k</h1>
-                        <span>Income</span>
-                    </div>
-                    <div>
-                        <span class="lab la-google-wallet"></span>
-    </div>
+        <div>
+            <h1>$6k</h1>
+            <span>Income</span>
+        </div>
+        <div>
+            <span class="lab la-google-wallet"></span>
+        </div>
     </div>
 </div>
 <!-- End Content -->
