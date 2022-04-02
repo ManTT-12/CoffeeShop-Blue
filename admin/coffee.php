bad1655db49eac828301fa77c8927248b1912fab
warbest101 <ttphuc75@gmail.com>
2022-04-02 13:17:08 +0700

    update

--- admin/coffee.php
+++ admin/coffee.php
@@ -2,6 +2,8 @@
 
 require_once('../env.php');
 require_once('../includes/Database.php');
+require_once("../includes/Session.php");
+Session::init();
 
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
