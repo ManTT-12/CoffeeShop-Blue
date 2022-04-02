6d6ece8fbdf7a02e6c19c36a797c14130bc1921b
warbest101 <ttphuc75@gmail.com>
2022-04-02 11:54:15 +0700

    crop layout

--- index.php
+++ index.php
@@ -6,3 +6,5 @@ require_once("env.php");
 // inlcudes
 require_once("includes/function.php");
 require_once("includes/Database.php");
+
+require_once('view/site/home.php');
---
+++ view/site_layout/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+
+    <!-- Bootstrap Library -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+    <!-- Jquery Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="public/css/layout.css">
+    <title>Coffee Shop</title>
+</head>
+
+<body>
+    <div id="navbar">
+        <div id="navbar-brand">
+            <h2><a href=""><span class="lab la-accusoft"><span></span>Coffee Shop</span></a></h2>
+        </div>
+        <div id="nav-menu-icon">
+            <span class="las la-bars"></span>
+        </div>
+        <nav id="navbar-menu">
+            <ul>
+                <li><a href=""><span><span></span>Product</span></a></li>
+                <li>
+                    <a href=""><span><span></span>About</span></a>
+                </li>
+                <li>
+                    <a href=""><span><span></span>Contract</span></a>
+                </li>
+                <li><a href=""><span class="las la-shopping-bag"><span></span>Order</span></a></li>
+                <li><a href=""><span class="las la-user-circle"><span></span>Account</span></a></li>
+                <div class="search-wrapper">
+                    <span class="las la-search"></span>
+                    <input type="search" placeholder="Search here">
+                </div>
+            </ul>
+        </nav>
+    </div>
+    <div id="nav-short-menu">
+        <ul>
+            <li><a href=""><span><span></span>Product</span></a></li>
+            <li><a href=""><span><span></span>About</span></a></li>
+            <li><a href=""><span><span></span>Contract</span></a></li>
+            <li><a href=""><span class="las la-shopping-bag"><span></span>Order</span></a></li>
+            <li><a href=""><span class="las la-user-circle"><span></span>Account</span></a></li>
+            <div class="search-wrapper">
+                <span class="las la-search"></span>
+                <input type="search" placeholder="Search here">
+            </div>
+        </ul>
+    </div>
+
+    <div id="main-content">
+        <section class="content">
