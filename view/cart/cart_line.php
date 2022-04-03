dbbb86dadf9f54af88b975e95acc8f2a70e547b5
ManTT-12 <lanhbang312@gmail.com>
2022-04-03 15:41:53 +0700

    update cart-line

--- view/cart/cart_line.php
+++ view/cart/cart_line.php
@@ -1,4 +1,26 @@
 <div class="item">
-    <div class="item-name">item1</div>
-    <div class="item-button"><i class="bi bi-x-lg"></i></div>
+    <div class="item-title">
+        <div class="item-title-info">
+            <div class="item-title-name">item1</div>
+            <div class="item-title-price">10$</div>
+        </div>
+        <div class="item-button-manager">
+            <div class="item-quantity">
+                <div class="item-quantity-plus"><i class="bi bi-plus-lg"></i></div>
+                <input type="number" min="1" step="1" class="item-amount">
+                <div class="item-quantity-minius"><i class="bi bi-dash-lg"></i></div>
+            </div>
+            <div class="item-button"><i class="bi bi-x-lg"></i></div>
+        </div>
+    </div>
+    <div class="item-details">
+        <div class="item-details-brand">
+            <div class="item-brand-title">Brand:</div>
+            <div class="item-brand-name">Brand1</div>
+        </div>
+        <div class="item-details-type">
+            <div class="item-type-title">Type:</div>
+            <div class="item-type-name">Type1</div>
+        </div>
+    </div>
 </div>
